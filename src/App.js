5dac27c285b664501f6f1b3061b4d96eaebad46f
github cjl248 <github cjl248@cornell.edu>
2019-11-07 00:15:47 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Calabria Pizza</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>UNDER CONSTRUCTION</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

1294130f11288d8d2db865a276d770307d3dd526
github cjl248 <github cjl248@cornell.edu>
2019-12-02 16:05:26 -0500

    navigation buttons

--- src/containers/navigation.jsx
+++ src/containers/navigation.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import {Button, Icon} from 'semantic-ui-react'
 
 
 export default class Navigation extends React.Component {
 
   render() {
     return (
-      <h1>NAV BAR</h1>
+      <div>
+
+        <Button animated='vertical'>
+          <Button.Content hidden>
+            <Icon name='home' />
+          </Button.Content>
+          <Button.Content visible>{"HOME"}</Button.Content>
+        </Button>
+
+        <Button animated='vertical'>
+          <Button.Content hidden>
+            <Icon name='food' />
+          </Button.Content>
+          <Button.Content visible>{"MENU"}</Button.Content>
+        </Button>
+
+        <Button animated='vertical'>
+          <Button.Content hidden>
+            <Icon name='truck' />
+          </Button.Content>
+          <Button.Content visible>{"CATERING"}</Button.Content>
+        </Button>
+
+        <Button animated='vertical'>
+          <Button.Content hidden>
+            <Icon name='shop' />
+          </Button.Content>
+          <Button.Content visible>{"ORDER"}</Button.Content>
+        </Button>
+
+      </div>
     )
   }
 

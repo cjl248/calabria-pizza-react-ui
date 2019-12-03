7b230277c68c5f8fcc4f7169a0f1a3c9f20c1b8f
github cjl248 <github cjl248@cornell.edu>
2019-12-02 22:06:00 -0500

    started banner and linked nav, main through app component

--- src/containers/main.jsx
+++ src/containers/main.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 
+import Home from './Home'
+import Menu from './Menu'
+import Catering from './Catering'
+import Order from './Order'
+
 export default class Main extends React.Component {
+
+  renderMain = () => {
+    if (this.props.activePage === "HOME") {
+      return (<Home />)
+    }
+    if (this.props.activePage === "MENU") {
+      return (<Menu />)
+    }
+    if (this.props.activePage === "CATERING") {
+      return (<Catering />)
+    }
+    if (this.props.activePage === "ORDER") {
+      return (<Order />)
+    }
+  }
 
   render() {
     return (
-      <>
-        <h1>MAIN</h1>
-      </>
+      this.renderMain()
     )
   }
+
 }
